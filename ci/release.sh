2f1ae54a5cdcf12b6293246bed207c6e363806e1
Dan Lebrero <dlebrero@gmail.com>
2017-12-06 17:24:26 +0100

    disabling gpg

--- ci/release.sh
+++ ci/release.sh
@@ -6,7 +6,6 @@ RELEASE_VERSION="1.${TRAVIS_BUILD_NUMBER}.${TRAVIS_COMMIT}"
 
 lein with-profile +set-version set-version $RELEASE_VERSION
 
-gpg --batch --passphrase ${CLOJARS_GPG_PASSWORD} --import devops.asc
-
+#gpg --batch --passphrase ${CLOJARS_GPG_PASSWORD} --import devops.asc
 #mvn deploy -s /app/maven-ci-settings.xml -Dgpg.passphrase=${CLOJARS_GPG_PASSWORD}
 lein deploy releases
